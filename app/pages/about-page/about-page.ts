190a7087f2caf3f7f7cef3f459dbe52d38dbe700
richdizz <richdizz@outlook.com>
2016-06-16 14:29:58 -0500

    updated comments

--- app/pages/about-page/about-page.ts
+++ app/pages/about-page/about-page.ts
@@ -18,6 +18,7 @@ export class AboutPage {
     let ref = cordova.InAppBrowser.open("https://outlook.office.com/connectors/Connect?state=myAppsState&app_id=4b543361-dbc2-4726-a351-c4b43711d6c5&callback_url=https://localhost:44300/callback", "_blank", "location=no,clearcache=yes,toolbar=yes");
     ref.addEventListener("loadstart", function(evt) {
       ctrl.zone.run(() => {
+        //listen for the callback URI
         if (evt.url.indexOf('https://localhost:44300/callback') == 0) {
           //decode the uri into the response parameters
           let parts = evt.url.split('&');
